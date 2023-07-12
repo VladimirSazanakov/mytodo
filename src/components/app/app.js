62cc647e0a610f1113210d31a841a58a237e1c48
Vladimir <sazanakov@inbox.ru>
2023-07-13 01:29:53 +0700

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello React</p>
+    </div>
+  );
+}
+
+export default App;
