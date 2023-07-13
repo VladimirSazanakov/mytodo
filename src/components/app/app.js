1739167ae3b47b2d7b8a8e7432f463812f7201fa
VladimirSazanakov <sazanakov@inbox.ru>
2023-07-14 01:11:14 +0700

    1.2.13

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { formatDistanceToNow } from 'date-fns';
 import './app.css';
 
 import Header from '../header/';
@@ -7,6 +8,9 @@ import TaskList from '../task-list';
 import Footer from '../footer/';
 
 function App() {
+  
+  //console.log(formatDistanceToNow(new Date(2023, 6, 13)));
+  //console.log(new Date());
 
   const todoData = [
     {label: 'Completed Tack', completed: true, editing: false, createdData: null},
