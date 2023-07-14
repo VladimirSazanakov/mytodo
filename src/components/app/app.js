dc35e96a162a1dfd214bdbfdf97f1d75ddd214c1
Vladimir <sazanakov@inbox.ru>
2023-07-15 01:43:36 +0700

    1.4.9 notwork

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,19 +9,47 @@ import Footer from '../footer/';
 
 export default class App extends Component {
 
+  count = 10;
+
+  state = {
+    todoData: [
+      { label: 'Completed Tack', completed: true, editing: false, createdData: null, id: 3 },
+      { label: 'Editing Tack', completed: false, editing: true, createdData: null, id: 2 },
+      { label: 'Active Tack', completed: false, editing: false, createdData: null, id: 1 },
+    ],
+  }
+
+  createNewTask = (label) => {
+    return {
+      label,
+      completed: false,
+      editing: false,
+      createdData: new Date,
+      id: this.count ++,
+
+    }
+  }
+
+  addNewTask = (label) =>{
+    const newTodoData = {...todoData, createNewTask(label)};
+    return newTodoData;
+  }
+
+
   render() {
 
+    /*
     const todoData = [
       { label: 'Completed Tack', completed: true, editing: false, createdData: null },
       { label: 'Editing Tack', completed: false, editing: true, createdData: null },
       { label: 'Active Tack', completed: false, editing: false, createdData: null },
     ];
-
+  */
     return (
       <div className="App">
         <Header />
         <section className='main'>
-          <TaskList todo={todoData} />
+          <TaskList todo={this.state.todoData} />
           <Footer />
         </section>
       </div>
