f66c5548741322b0817bd055ed53ff850bf64278
Vladimir <sazanakov@inbox.ru>
2023-07-18 02:09:29 +0700

    1.4.20 alpha2

--- src/components/task-list/task-list.js
+++ src/components/task-list/task-list.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import './task-list.css';
 
@@ -6,65 +6,75 @@ import Task from '../task/';
 
 export default class TaskList extends Component {
 
-  onSubmitEdit = (event) =>{
+  state = { editingTask: [] };
+
+  onSubmitEdit = (event) => {
     event.preventDefault();
     //console.log(event.target);
     //console.log('Edit Task', event.target.editInput.value);
     //console.log(event.target.editInput);
+    console.log(this.state);
     this.props.onEditSubmit(event.target.editInput.dataset.id, event.target.editInput.value);
   };
-  
-  onChangeEditing = (event) =>{
+
+  onChangeEditing = (event) => {
     let value = event.target.value;
     console.log(event.target.value);
     event.target.value += value;
 
   }
 
- render (){
-    const {todo, filter, onCompleted, onDeleteTask, onEditBtn} = this.props;
+  addToEditingTask = (id, value) => {
+    this.setState(({ editingTask }) => {
+      const newTask = { id, value };
+      const newArr = [...editingTask, newTask];
+      return ({ editingTask: newArr });
+    })
+  }
+
+  render() {
+    const { todo, filter, onCompleted, onDeleteTask, onEditBtn } = this.props;
 
     const elements = todo.map(task => {
       const liClassName = task.completed ? 'completed' : task.editing ? 'editing' : '';
-      let label = task.label;
       
       const inputEdit = task.editing ? <form onSubmit={this.onSubmitEdit}>
-                                          <input type="text"
-                                                  name="editInput"
-                                                  data-id = {task.id} 
-                                              className="edit" 
-                                              value={label}
-                                              onChange={this.onChangeEditing}
-                                              >
-                                        </input>
-                                        </form> : null;
-      
-      console.log(filter);
-      const valid = (filter == 'All')||
-                    (filter=='Active'&&!task.completed)||
-                    (filter=='Completed'&&task.completed); 
-     
-      if(valid){
+        <input type="text"
+          name="editInput"
+          data-id={task.id}
+          className="edit"
+          value={task.value}
+          onChange={this.onChangeEditing}
+        >
+        </input>
+      </form> : null;
 
-      return (
-        <li key={task.id} className={liClassName}>
-          <Task label={task.label} 
-                createdDate={task.createdDate} 
-                completed={task.completed} 
-                onChecked={()=>onCompleted(task.id)}
-                onDeleteTask={()=>onDeleteTask(task.id)}
-                onEditBtn={()=>onEditBtn(task.id)}/>
-          {inputEdit}
-        </li>
-      )
+      console.log(filter);
+      const valid = (filter == 'All') ||
+        (filter == 'Active' && !task.completed) ||
+        (filter == 'Completed' && task.completed);
+
+      if (valid) {
+
+        return (
+          <li key={task.id} className={liClassName}>
+            <Task label={task.label}
+              createdDate={task.createdDate}
+              completed={task.completed}
+              onChecked={() => onCompleted(task.id)}
+              onDeleteTask={() => onDeleteTask(task.id)}
+              onEditBtn={() => onEditBtn(task.id)} />
+            {inputEdit}
+          </li>
+        )
       }
     });
 
-  return (
-    <ul className="todo-list">
-      {elements}
-    </ul>
-  );
-};
+    return (
+      <ul className="todo-list">
+        {elements}
+      </ul>
+    );
+  };
 };
 
